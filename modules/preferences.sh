dcb863fa79473fd3550f8921310b97cb81ddacc4
Julien Ruaux <jruaux@users.noreply.github.com>
2024-09-21 21:49:58 -0700

    Update preferences.sh

--- modules/preferences.sh
+++ modules/preferences.sh
@@ -11,7 +11,6 @@ defaults write com.apple.dock "show-recents" -bool false # Disable recents
 defaults write com.apple.dock static-only -bool true # only show open apps on dock
 
 # Finder
-defaults write com.apple.finder "AppleShowAllFiles" -bool true # Show hidden files
 defaults write com.apple.finder "ShowPathbar" -bool true # Show path bar
 defaults write com.apple.finder "ShowStatusBar" -bool true # Show status bar
 defaults write com.apple.finder "FXPreferredViewStyle" -string Nlsv # List view
@@ -20,20 +19,12 @@ defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf" # Search s
 defaults write com.apple.finder "CreateDesktop" -bool true # Disable desktop icons
 defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool false # Disable drive icon on desktop
 
-# Security
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on # Enable firewall
-
 # Screencapture 
 defaults write com.apple.screencapture "location" -string "~/Pictures"
-
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool true # Show file extensions
 
 # Privacy
 defaults write com.apple.AdLib.plist allowApplePersonalizedAdvertising -bool false
 defaults write com.apple.AdLib.plist allowIdentifierForAdvertising -bool false
 defaults write com.apple.AdLib.plist personalizedAdsMigrated -bool false
-
-# Set hostname
-sudo scutil --set HostName jay-macbook
 
 killall Dock && killall Finder && killall SystemUIServer # Restart dock and finder to apply changes
